c5a20d27e8cf5b59dd3f86cf3b482b12dd4cdf7a
jaffe9 <jaffar.meeran@gmail.com>
2024-12-20 13:23:57 +0300

    Added Log

--- src/apis/services/auth.service.js
+++ src/apis/services/auth.service.js
@@ -12,6 +12,7 @@ const ApiError = require('../../utils/api-error')
  * @returns {Promise<User>}
  */
 const loginUserWithEmailAndPassword = async (email, password) => {
+    console.log(email)
     const user = await userService.getUserByEmail(email)
     if (!user || !(await user.isPasswordMatch(password))) {
         throw new ApiError(httpStatus.UNAUTHORIZED, 'Incorrect email or password')
