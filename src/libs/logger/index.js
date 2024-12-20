0bbba268eadc46b155da6580e7a591e769bcf18e
jaffe9 <jaffar.meeran@gmail.com>
2024-12-20 16:29:37 +0300

    Adding logs

--- src/libs/logger/index.js
+++ src/libs/logger/index.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const winston = require('winston')
+const { combine, timestamp, json } = winston.format
 const logger = winston.createLogger({
     level: process.env.LOG_LEVEL || 'info',
     format: combine(
