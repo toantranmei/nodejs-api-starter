aff29ee80cf528a66316a9824d9a9948192bfc57
jaffe9 <jaffar.meeran@gmail.com>
2025-01-09 22:23:07 +0300

    Login API Changes

--- src/apis/services/user.service.js
+++ src/apis/services/user.service.js
@@ -5,13 +5,16 @@ const { User } = require('../models')
 const { supabase } = require('../../configs/env')
 const Logger = require('../../libs/logger')
 const log = new Logger()
-const headers = {
-    'Content-Type': 'application/json',
-    Authorization:
-        'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpocGxrdGFvdnB5ZW5teXBranFsIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY5MjUxOTYzMywiZXhwIjoyMDA4MDk1NjMzfQ.i-QsgcR7aZTxpubO0dHGPs-li50B7GrVQKsuW866YLA',
-}
-const params = {
-    apikey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpocGxrdGFvdnB5ZW5teXBranFsIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY5MjUxOTYzMywiZXhwIjoyMDA4MDk1NjMzfQ.i-QsgcR7aZTxpubO0dHGPs-li50B7GrVQKsuW866YLA',
+let config = {
+    method: 'post',
+    maxBodyLength: Infinity,
+    url: 'https://zhplktaovpyenmypkjql.supabase.co/auth/v1/token?grant_type=password&apikey=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpocGxrdGFvdnB5ZW5teXBranFsIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY5MjUxOTYzMywiZXhwIjoyMDA4MDk1NjMzfQ.i-QsgcR7aZTxpubO0dHGPs-li50B7GrVQKsuW866YLA',
+    headers: {
+        'Content-Type': 'application/json',
+        Authorization:
+            'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpocGxrdGFvdnB5ZW5teXBranFsIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY5MjUxOTYzMywiZXhwIjoyMDA4MDk1NjMzfQ.i-QsgcR7aZTxpubO0dHGPs-li50B7GrVQKsuW866YLA',
+    },
+    data: data,
 }
 /**
  * Create a user
@@ -35,21 +38,21 @@ const getUserByEmail = async (email, password) => {
     //return User.findOne({ email })
 }
 
-function login(email, password) {
+function login(emailVal, passwordVal) {
     log.info(supabase.loginUrl)
     log.info(email)
     log.info(password)
-    const response = axios.post(
-        supabase.loginUrl,
-        {
-            headers: headers,
-            params: params,
-        },
-        {
-            email: email,
-            password: password,
-        }
-    )
+    config.data = { email: emailVal, password: passwordVal }
+    const response = axios
+        .request(config)
+        .then((response) => {
+            console.log(JSON.stringify(response.data))
+        })
+        .catch((error) => {
+            console.log(error)
+            log.error(error)
+            log.info(error)
+        })
     log.info(response)
     return response
 }
