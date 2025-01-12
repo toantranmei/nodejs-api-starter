39978430686b93a69b155fe4c8810d6998f17131
jaffe9 <jaffar.meeran@gmail.com>
2025-01-12 22:43:53 +0300

    Leave API Changes

--- src/apis/routes/index.js
+++ src/apis/routes/index.js
@@ -5,6 +5,7 @@ const taskRoute = require('./v1/task.route')
 const userRoute = require('./v1/user.route')
 const statusRoute = require('./v1/status.route')
 const payslipRoute = require('./v1/payslip.route')
+const leaveRoute = require('./v1/leave.route')
 
 const router = express.Router()
 
@@ -26,8 +27,12 @@ const defaultRoutes = [
         route: statusRoute,
     },
     {
-	path: '/v1/payslip',
-	route: payslipRoute,
+        path: '/v1/payslip',
+        route: payslipRoute,
+    },
+    {
+        path: '/v1/leave',
+        route: leaveRoute,
     },
 ]
 
